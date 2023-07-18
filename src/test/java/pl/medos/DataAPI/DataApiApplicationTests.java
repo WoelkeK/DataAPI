5294150f1c52e3cfc01aa03a8cd717fcecfb90b5
krzysztof.woelke <krzysiek@woelke.pl>
2023-07-18 15:58:55 +0200

    Init structure for project machines data processing

---
+++ src/test/java/pl/medos/DataAPI/DataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.medos.DataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
